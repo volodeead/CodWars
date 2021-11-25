628dfda673820782fb0f64f20c56821f9a8c30b3
volodeead <volodeead.v@gmail.com>
2021-11-25 16:49:23 +0200

    2 commit

---
+++ ConvertaStringToaNumber/src/kata/ConvertaStringToaNumber.java
@@ -0,0 +1,15 @@
+package kata;
+
+public class ConvertaStringToaNumber {
+
+	  /*
+	  We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+	Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+	  */
+	  
+	  public static int stringToNumber(String str) {
+	    return Integer.parseInt(str);
+	  }
+	
+}
